402014d32e76ed5733d8a96c402b8aa753961551
boghetim <boghe-tim@hotmail.be>
2023-12-12 10:21:14 +0100

    UDP server vitis

---
+++ vraag2/vitis/design_1_wrapper/platform.tcl
@@ -0,0 +1,22 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\boghe\EOS_opdracht3eai\vraag2\vitis\design_1_wrapper\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\boghe\EOS_opdracht3eai\vraag2\vitis\design_1_wrapper\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {design_1_wrapper}\
+-hw {C:\Users\boghe\EOS_opdracht3eai\vraag2\design_1_wrapper.xsa}\
+-out {C:/Users/boghe/EOS_opdracht3eai/vraag2/vitis}
+
+platform write
+domain create -name {freertos10_xilinx_ps7_cortexa9_0} -display-name {freertos10_xilinx_ps7_cortexa9_0} -os {freertos10_xilinx} -proc {ps7_cortexa9_0} -runtime {cpp} -arch {32-bit} -support-app {freertos_lwip_udp_perf_server}
+platform generate -domains 
+platform active {design_1_wrapper}
+domain active {zynq_fsbl}
+domain active {freertos10_xilinx_ps7_cortexa9_0}
+platform generate -quick
+platform generate
